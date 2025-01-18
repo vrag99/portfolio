6b9482cf143c94c637df94a1448a1ae8b66e9d87
Garv Makkar <garv.codes@gmail.com>
2025-01-19 03:41:22 +0530

    abstract command menu into a new component

--- components/home/ask-box.tsx
+++ components/home/ask-box.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { AnimatePresence, motion } from "motion/react";
+import CommandMenu from "./command-menu";
+import { Command } from "@/lib/types";
 
-interface Command {
-  name: string;
-  description: string;
-}
-
-const commands: Command[] = [
+const COMMANDS: Command[] = [
   {
     name: "/about",
     description: "know more about me",
@@ -29,19 +26,14 @@ const commands: Command[] = [
   },
 ];
 
-export default function AskBox() {
+const AskBox = () => {
   const [inputValue, setInputValue] = useState("");
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [filteredCommands, setFilteredCommands] = useState<Command[]>([]);
-  const [selectedIndex, setSelectedIndex] = useState(0);
   const [hasSelectedCommand, setHasSelectedCommand] = useState(false);
 
   useEffect(() => {
     if (inputValue.startsWith("/") && !hasSelectedCommand) {
       setIsMenuOpen(true);
-      setFilteredCommands(
-        commands.filter((command) => command.name.includes(inputValue))
-      );
     } else {
       setIsMenuOpen(false);
     }
@@ -52,36 +44,6 @@ export default function AskBox() {
     setHasSelectedCommand(true);
     setInputValue(command.name);
   };
-
-  const handleKeyDown = useCallback(
-    (e: KeyboardEvent) => {
-      switch (e.key) {
-        case "ArrowDown":
-          setSelectedIndex((prev) =>
-            Math.min(prev + 1, filteredCommands.length - 1)
-          );
-          break;
-        case "ArrowUp":
-          setSelectedIndex((prev) => Math.max(prev - 1, 0));
-          break;
-        case "Enter":
-          handleCommandSelect(filteredCommands[selectedIndex]);
-          break;
-        default:
-          break;
-      }
-    },
-    [selectedIndex, filteredCommands]
-  );
-
-  useEffect(() => {
-    if (isMenuOpen) {
-      document.addEventListener("keydown", handleKeyDown);
-    }
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [handleKeyDown, isMenuOpen]);
 
   return (
     <div className="relative flex flex-col">
@@ -103,57 +65,16 @@ export default function AskBox() {
         }}
         placeholder={`Ask me anything :) | Type "/" for commands`}
       />
-      <AnimatePresence>
-        {isMenuOpen && (
-          <motion.div
-            initial={{ opacity: 0, y: -10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 10 }}
-            layout
-            transition={{
-              opacity: { duration: 0.2 },
-              layout: {
-                type: "spring",
-                bounce: 0.25,
-                duration: 0.5,
-              },
-            }}
-            className={cn(
-              "w-full",
-              "rounded-2xl",
-              "absolute top-16"
-            )}
-          >
-            <div className="flex flex-col gap-2">
-              {filteredCommands.map((command, index) => (
-                <button
-                  key={command.name}
-                  className={cn(
-                    "grid grid-cols-7 gap-4",
-                    "rounded-2xl px-4 py-2",
-                    "border border-muted",
-                    "transition-all duration-300",
-                    selectedIndex === index
-                      ? "bg-secondary/10 border-b-2 border-b-input"
-                      : "hover:bg-secondary/5"
-                  )}
-                  onClick={() => handleCommandSelect(command)}
-                >
-                  <p
-                    className="col-span-2 text-left font-highlight italic"
-                    onClick={() => handleCommandSelect(command)}
-                  >
-                    {command.name}
-                  </p>
-                  <p className="col-span-5 text-sm text-left text-muted-foreground">
-                    {command.description}
-                  </p>
-                </button>
-              ))}
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <CommandMenu
+        isOpen={isMenuOpen}
+        setIsOpen={setIsMenuOpen}
+        commands={COMMANDS}
+        search={inputValue}
+        selectCommand={handleCommandSelect}
+        position="bottom"
+      />
     </div>
   );
-}
+};
+
+export default AskBox;
---
+++ components/home/command-menu.tsx
@@ -0,0 +1,135 @@
+import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
+
+import { AnimatePresence } from "motion/react";
+import { useCallback } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Command } from "@/lib/types";
+
+interface CommandMenuProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  commands: Command[];
+  search: string;
+  selectCommand: (command: Command) => void;
+  position: "top" | "bottom";
+}
+
+const CommandMenu = ({
+  isOpen,
+  setIsOpen,
+  commands,
+  search,
+  selectCommand,
+  position = "bottom",
+}: CommandMenuProps) => {
+  const [filteredCommands, setFilteredCommands] = useState<Command[]>(commands);
+  useEffect(() => {
+    setFilteredCommands(
+      commands.filter((command) => command.name.includes(search))
+    );
+  }, [search]);
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowDown":
+          if (position === "bottom") {
+            setSelectedIndex((prev) =>
+              Math.min(prev + 1, filteredCommands.length - 1)
+            );
+          } else {
+            setSelectedIndex((prev) => Math.max(prev - 1, 0));
+          }
+          break;
+        case "ArrowUp":
+          if (position === "bottom") {
+            setSelectedIndex((prev) => Math.max(prev - 1, 0));
+          } else {
+            setSelectedIndex((prev) =>
+              Math.min(prev + 1, filteredCommands.length - 1)
+            );
+          }
+          break;
+        case "Enter":
+          selectCommand(filteredCommands[selectedIndex]);
+          break;
+        default:
+          break;
+      }
+    },
+    [selectedIndex, filteredCommands]
+  );
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("keydown", handleKeyDown);
+    }
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown, isOpen]);
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0, y: position === "bottom" ? -10 : 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: position === "bottom" ? 10 : -10 }}
+          layout
+          transition={{
+            opacity: { duration: 0.2 },
+            layout: {
+              type: "spring",
+              bounce: 0.25,
+              duration: 0.5,
+            },
+          }}
+          className={cn(
+            "w-full",
+            "rounded-2xl",
+            "absolute backdrop-blur-lg bg-background",
+            position === "bottom" ? "top-16" : "bottom-16"
+          )}
+        >
+          <div
+            className={cn(
+              "flex gap-2",
+              position === "bottom" ? "flex-col" : "flex-col-reverse"
+            )}
+          >
+            {filteredCommands.map((command, index) => (
+              <button
+                key={command.name}
+                className={cn(
+                  "grid grid-cols-7 gap-4",
+                  "rounded-2xl px-4 py-2",
+                  "border border-muted",
+                  "transition-all duration-300",
+                  selectedIndex === index
+                    ? "bg-secondary/10 border-b-2 border-b-input"
+                    : "hover:bg-secondary/5"
+                )}
+                onClick={() => selectCommand(command)}
+              >
+                <p
+                  className="col-span-2 text-left font-highlight italic"
+                  onClick={() => selectCommand(command)}
+                >
+                  {command.name}
+                </p>
+                <p className="col-span-5 text-sm text-left text-muted-foreground">
+                  {command.description}
+                </p>
+              </button>
+            ))}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default CommandMenu;
