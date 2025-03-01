e33a45421c56b12d69c0793b53aa458ee4a43cab
Garv Makkar <garv.codes@gmail.com>
2025-03-01 12:45:55 +0530

    add timeline renderer

--- components/chat/renderer/timeline-renderer.tsx
+++ components/chat/renderer/timeline-renderer.tsx
@@ -1,8 +1,37 @@
+import {
+  Timeline,
+  TimelineItem,
+  TimelineTitle,
+  TimelineDescription,
+  TimelineTime,
+  TimelineHeader,
+  TimelineItemType,
+} from "@/components/ui/timeline";
 import { Achievement } from "@/lib/types";
-import React from "react";
 
 const TimelineRenderer = ({ timeline }: { timeline: Achievement[] }) => {
-  return <div>TimelineRenderer</div>;
+  return (
+    <Timeline>
+      {timeline.map((item, i) => (
+        <TimelineItem key={i}>
+          <TimelineHeader>
+            <TimelineTime variant={"outline"}>
+              {item.timestamp.toLocaleString("en-US", {
+                month: "short",
+                year: "numeric",
+              })}
+            </TimelineTime>
+            <TimelineTitle>{item.title}</TimelineTitle>
+          </TimelineHeader>
+          {item.description && (
+            <TimelineDescription className="font-medium">
+              {item.description}
+            </TimelineDescription>
+          )}
+        </TimelineItem>
+      ))}
+    </Timeline>
+  );
 };
 
 export default TimelineRenderer;
