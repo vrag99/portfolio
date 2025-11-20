cee0eecbf9656e4d17593e622c6b71ae9301936c
Garv Makkar <garv.codes@gmail.com>
2025-11-20 15:56:21 +0530

    update experiences

--- content/portfolio/experience.ts
+++ content/portfolio/experience.ts
@@ -2,6 +2,21 @@ import { Background } from "@/lib/types";
 
 export const EXPERIENCE: Background[] = [
   {
+    title: "Cohort Fellow @UniswapHookIncubator",
+    description:
+      "Selected as one of the top 100 fellows worldwide for the Uniswap Hook Incubator program. Working on building innovative solutions in the DeFi space using Uniswap's Hook protocol.",
+    startTime: "2024-08",
+    link: "https://atrium.academy/uniswap",
+  },
+  {
+    title: "Founding Engineer @StealthAI Startup",
+    description:
+      "Worked as a founding engineer at a New York based stealth AI startup, and worked on every aspect of the product from component libraries to deployment pipelines.",
+    startTime: "2024-06",
+    endTime: "2024-09",
+    link: "#",
+  },
+  {
     title: "Developer @SDSLabs",
     description:
       "SDSLabs is astudent-run tech organization at IIT Roorkee. I am a developer here and contributed to various projects and participated in quite a lot of hackathons.",
